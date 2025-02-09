24414338185f5a92947f8ca9fce84e76963bc4a2
Егор <root@apiteam.ru>
2025-02-09 08:14:15 +0300

    v0.0.7

--- src/hook/onEvent.tsx
+++ src/hook/onEvent.tsx
@@ -1,9 +1,9 @@
 import { onCleanup, onMount } from "solid-js";
 import { CTX, Callback, Events } from "../types";
 
-function onEvents<E extends Events>(this: CTX, callback: Callback<E, "response">) {
-    onMount(() => this.callbackEvents.add(callback))
-    onCleanup(() => this.callbackEvents.delete(callback))
-};
+function onEvents<E extends Events, ER extends unknown>(this: CTX, callback: Callback<E, "response", ER>) {
+  onMount(() => this.callbackEvents.add(callback));
+  onCleanup(() => this.callbackEvents.delete(callback));
+}
 
 export default onEvents;
--- src/types.ts
+++ src/types.ts
@@ -1,58 +1,65 @@
 import { Accessor, Setter } from "solid-js";
 
 export enum Status {
-    CONNECTING,
-    OPEN,
-    CLOSE,
-    ABORT,
-    USER_CLOSED
+  CONNECTING,
+  OPEN,
+  CLOSE,
+  ABORT,
+  USER_CLOSED,
 }
 
-export type Events = Record<string, Record<"request" | "response" | string, any>>
+export type Events = Record<string, Record<"request" | "response" | string, any>>;
 
 export type Data<E extends Events, T extends "request" | "response", ER extends unknown> = {
-    [K in keyof E]: {
-        event: K, data: {
-            error: ER,
-            response: E[K][T]
-        }
-    }
-}[keyof E]
+  [K in keyof E]: {
+    event: K;
+    data: Result<ER, E[K][T]>;
+  };
+}[keyof E];
 
-export type Callback<E extends Events, T extends "request" | "response", ER extends unknown> = (data: Data<E, T, ER>) => void;
+export type Callback<E extends Events, T extends "request" | "response", ER extends unknown> = (
+  data: Data<E, T, ER>,
+) => void;
+
+export type CallbackEmitter<E extends Events, T extends "request" | "response", ER extends unknown> = (
+  data: Data<E, T, ER>["data"],
+) => void;
 
 export type CTX = {
+  url: string;
+  autoConnect: boolean;
+  autoReconnect: boolean;
 
-    url: string;
-    autoConnect: boolean;
-    autoReconnect: boolean;
+  status: Accessor<Status>;
+  setStatus: Setter<Status>;
 
-    status: Accessor<Status>;
-    setStatus: Setter<Status>;
-
-    client: Worker;
-    requestID: number;
-    callbackEmitter: Map<number, Callback<any, "response", any>>;
-    callbackEvents: Set<Callback<any, "response", any>>;
-
-}
+  client: Worker;
+  requestID: number;
+  callbackEmitter: Map<number, CallbackEmitter<any, "response", any>>;
+  callbackEvents: Set<Callback<any, "response", any>>;
+};
 
 export type Context<E extends Events, ER extends unknown> = {
-    status: Accessor<Status>,
-    connect: () => void;
-    disconnect: () => void;
-    terminate: () => void;
-    send: {
-        <K extends keyof E>(event: K, data: E[K]["request"]): Promise<E[K]["response"]>
-        <K extends keyof E>(event: K, data: E[K]["request"]): Promise<E[K]["response"]>
-        <K extends keyof E>(event: K, data: E[K]["request"], callback: (data: E[K]["response"]) => void): void
-        <K extends keyof E>(event: K, data: E[K]["request"], callback: (data: E[K]["response"]) => void): void
-    };
-    onEvents: (callback: Callback<E, "response", ER>) => void;
+  status: Accessor<Status>;
+  connect: () => void;
+  disconnect: () => void;
+  terminate: () => void;
+  send: {
+    <K extends keyof E>(event: K, data: E[K]["request"]): Promise<Result<ER, E[K]["response"]>>;
+    <K extends keyof E>(event: K, data: E[K]["request"]): Promise<Result<ER, E[K]["response"]>>;
+    <K extends keyof E>(event: K, data: E[K]["request"], callback: (data: Result<ER, E[K]["response"]>) => void): void;
+    <K extends keyof E>(event: K, data: E[K]["request"], callback: (data: Result<ER, E[K]["response"]>) => void): void;
+  };
+  onEvents: (callback: Callback<E, "response", ER>) => void;
 };
 
 export type Options = {
-    url: string;
-    autoConnect: boolean;
-    autoReconnect: boolean;
-}
+  url: string;
+  autoConnect: boolean;
+  autoReconnect: boolean;
+};
+
+export type Result<E, R> = {
+  error: E;
+  response: R;
+};
