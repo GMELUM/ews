5d3e0778320a47ce3a034b9c160fe6fe9a45bba9
GMELUM <gmelum@vk.com>
2025-02-21 13:58:27 +0300

    v0.0.15

--- src/types.ts
+++ src/types.ts
@@ -20,20 +20,20 @@ type StaticMethods = {
   },
 }
 
-export type Events = Record<string, Record<"request" | "response" | string, any>>;
+export type Events = Record<string, Record<"request" | "response" | "event" | string, any>>;
 
-export type Data<E extends Events, T extends "request" | "response", ER extends unknown> = {
+export type Data<E extends Events, T extends "request" | "response" | "event", ER extends unknown> = {
   [K in keyof E]: {
     event: K;
     data: Result<ER, E[K][T]>;
   };
 }[keyof E];
 
-export type Callback<E extends Events, T extends "request" | "response", ER extends unknown> = (
+export type Callback<E extends Events, T extends "request" | "response" | "event", ER extends unknown> = (
   data: Data<E, T, ER>,
 ) => void;
 
-export type CallbackEmitter<E extends Events, T extends "request" | "response", ER extends unknown> = (
+export type CallbackEmitter<E extends Events, T extends "request" | "response" | "event", ER extends unknown> = (
   data: Data<E, T, ER>["data"],
 ) => void;
 
@@ -62,7 +62,7 @@ export type Context<E extends Events, ER extends unknown> = {
     <K extends keyof E>(event: K, data: E[K]["request"], callback: (data: Result<ER, E[K]["response"]>) => void): void;
     <K extends keyof E>(event: K, data: E[K]["request"], callback: (data: Result<ER, E[K]["response"]>) => void): void;
   };
-  onEvents: (callback: Callback<E & StaticMethods, "response", ER>) => void;
+  onEvents: (callback: Callback<E & StaticMethods, "event", ER>) => void;
 };
 
 export type Options = {
